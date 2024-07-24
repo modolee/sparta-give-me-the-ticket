ee2fef2430e198f7bbe04f9463cc61b7dbf7053d
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-07-24 21:59:04 +0900

    46-Update/Create_used_trade_board_START

--- src/modules/trades/trades.module.ts
+++ src/modules/trades/trades.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TradesService } from './trades.service';
 import { TradesController } from './trades.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Trade } from '../../entities/trades/trade.entity';
+import { TradeLog } from '../../entities/trades/trade-log.entity';
+import { Show } from 'src/entities/shows/show.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Trade, TradeLog, Show])],
   providers: [TradesService],
   controllers: [TradesController],
 })
