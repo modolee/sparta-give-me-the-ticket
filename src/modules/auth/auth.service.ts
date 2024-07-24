dd786774e29320aaef58a8eb94d2f039b4899b74
jkc-mycode <jkc9709@naver.com>
2024-07-24 22:33:27 +0900

    Add/sign in api

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,14 +1,24 @@
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/users/user.entity';
 import { Repository } from 'typeorm';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly jwtService: JwtService
+  ) {}
 
   // 회원가입
   async signUp(signUpDto: SignUpDto) {
@@ -20,7 +30,7 @@ export class AuthService {
     }
 
     // 이메일 중복 체크
-    let existedUser = await this.usersRepository.find({
+    let existedUser = await this.usersRepository.findOne({
       where: { email },
     });
     if (existedUser) {
@@ -28,7 +38,7 @@ export class AuthService {
     }
 
     // 닉네임 중복 체크
-    existedUser = await this.usersRepository.find({
+    existedUser = await this.usersRepository.findOne({
       where: { nickname },
     });
     if (existedUser) {
@@ -48,9 +58,38 @@ export class AuthService {
     return user;
   }
 
+  // 사용자 유효성 검사
+  async validateUser({ email, password }) {
+    const user = await this.usersRepository.findOne({
+      where: { email },
+    });
+
+    if (!user || user.deletedAt) {
+      throw new NotFoundException('일치하는 사용자가 없습니다.');
+    }
+
+    // 암호화된 비밀번호 검사
+    const isComparePassword = await compare(password, user.password);
+    if (!isComparePassword) {
+      throw new UnauthorizedException('인증된 사용자가 아닙니다.');
+    }
+
+    return user;
+  }
+
   // 로그인
-  async signIn(signInDto: SignInDto) {
-    return;
+  async signIn(user: User) {
+    // 토큰 발급
+    const accessToken = this.jwtService.sign({ id: user.id });
+    const refreshToken = this.jwtService.sign(
+      { id: user.id },
+      { secret: process.env.REFRESH_SECRET_KEY, expiresIn: '7d' }
+    );
+
+    // 리프레시 토큰 저장
+    // await this.usersRepository.update({ id: user.id }, { refreshToken });
+
+    return { accessToken, refreshToken };
   }
 
   // 로그아웃
