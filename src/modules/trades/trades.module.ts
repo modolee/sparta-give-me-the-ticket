73cee29d731cbce4aae231d9f8a520efc96fe3fa
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-07-24 22:49:39 +0900

    Update/Complete_working_create_of_detail_boardview!

--- src/modules/trades/trades.module.ts
+++ src/modules/trades/trades.module.ts
@@ -6,9 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Trade } from '../../entities/trades/trade.entity';
 import { TradeLog } from '../../entities/trades/trade-log.entity';
 import { Show } from 'src/entities/shows/show.entity';
+import { Schedule } from 'src/entities/shows/schedule.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Trade, TradeLog, Show])],
+  imports: [TypeOrmModule.forFeature([Trade, TradeLog, Show, Schedule])],
   providers: [TradesService],
   controllers: [TradesController],
 })
