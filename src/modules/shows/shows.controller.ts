67ea27084c05cf1c80820c0785601f8322e19386
komiharuu <biomilky3@gmail.com>
2024-07-24 20:10:49 +0900

    Update/create bookmark

--- src/modules/shows/shows.controller.ts
+++ src/modules/shows/shows.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestj
 import { CreateShowDto } from './dto/create-show.dto';
 import { ShowsService } from 'src/modules/shows/shows.service';
 import { ShowCategory } from 'src/commons/types/shows/show-category.type';
+import { User } from 'src/entities/users/user.entity';
 
 @Controller('shows')
 export class ShowsController {
@@ -9,7 +10,7 @@ export class ShowsController {
 
   /**
    *  공연 생성
-   * @Param 
+   * @Param
    * @returns
    * */
   @Post()
@@ -19,11 +20,11 @@ export class ShowsController {
 
   /**
    *  공연 목록 조회
-   * */ 
+   * */
   @Get()
-  getShowList(@Query('category') category: string, @Query('search') title: string) { 
-    return this.showsService.getShowList(category,title)
-    }
+  getShowList(@Query('category') category: string, @Query('search') title: string) {
+    return this.showsService.getShowList(category, title);
+  }
 
   /**
    * 공연 상세 조회
@@ -55,11 +56,11 @@ export class ShowsController {
    * @returns
    */
   @Post(':showId/bookmark')
-  async createBookmark(@Param('showId') showId: number) {
-    return this.showsService.createBookmark(showId);
-    }
-    
-     /**
+  async createBookmark(@Param('showId') showId: number, user: User) {
+    return this.showsService.createBookmark(showId, user);
+  }
+
+  /**
    * 공연 찜하기 취소
    * @param showId
    * @returns
@@ -68,23 +69,23 @@ export class ShowsController {
   async deleteBookmark(@Param('showId') showId: number) {
     return this.showsService.deleteBookmark(showId);
   }
-     /**
+  /**
    * 티켓 예매
    * @param showId
    * @returns
    */
-    @Post(':showId/ticket')
-    async createTicket(@Param('showId') showId: number) {
-        return this.showsService.createTicket(showId);
-    }
-      /**
+  @Post(':showId/ticket')
+  async createTicket(@Param('showId') showId: number) {
+    return this.showsService.createTicket(showId);
+  }
+  /**
    * 티켓 환불
    * @param showId
    * @param ticketId
    * @returns
    */
-    @Delete(':showId/ticket/:ticketId')
-    async refundTicket(@Param('showId') showId: number, @Param('ticketId') ticketId: number) {
-        return this.showsService.refundTicket(showId, ticketId);
-    }
+  @Delete(':showId/ticket/:ticketId')
+  async refundTicket(@Param('showId') showId: number, @Param('ticketId') ticketId: number) {
+    return this.showsService.refundTicket(showId, ticketId);
+  }
 }
