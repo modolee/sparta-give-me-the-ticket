3999137a2fbfa39d4b0a1d5a5f9012d2c797343d
jkc-mycode <jkc9709@naver.com>
2024-07-26 01:30:33 +0900

    Bugfix/ec2 cicd error test

--- src/modules/auth/dto/sign-up.dto.ts
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -5,7 +5,7 @@ import { User } from 'src/entities/users/user.entity';
 export class SignUpDto extends PickType(User, ['email', 'password', 'nickname']) {
   /**
    * 비밀번호 확인
-   * @example "123123"
+   * @example "Test1234!"
    */
   @IsString()
   @IsNotEmpty({ message: '비밀번호 확인을 입력해 주세요.' })
