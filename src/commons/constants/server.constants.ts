83daf722e49c0ac7ffe7b62173c848f784c68b5b
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-07-30 11:17:04 +0900

    update:bullqueue_module_initiailize

---
+++ src/commons/constants/server.constants.ts
@@ -0,0 +1,7 @@
+export const SERVER = {
+  PORT: process.env.SERVER_PORT,
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DATABASE: process.env.DB_NAME,
+};
--- src/modules/trades/trades.module.ts
+++ src/modules/trades/trades.module.ts
@@ -2,17 +2,33 @@ import { Module } from '@nestjs/common';
 import { TradesService } from './trades.service';
 import { TradesController } from './trades.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 
+//entities
 import { Trade } from '../../entities/trades/trade.entity';
 import { TradeLog } from '../../entities/trades/trade-log.entity';
 import { Show } from 'src/entities/shows/show.entity';
 import { Schedule } from 'src/entities/shows/schedule.entity';
 import { Ticket } from 'src/entities/shows/ticket.entity';
 import { User } from 'src/entities/users/user.entity';
+import { QueueConfig } from 'src/configs/queue.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Trade, TradeLog, Show, Schedule, Ticket, User])],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => QueueConfig.createQueueConfig(ConfigService),
+    }),
+    BullModule.registerQueue({
+      name: 'ticketQueue',
+    }),
+    TypeOrmModule.forFeature([Trade, TradeLog, Show, Schedule, Ticket, User]),
+  ],
   providers: [TradesService],
   controllers: [TradesController],
+  exports: [TypeOrmModule, BullModule],
 })
 export class TradesModule {}
