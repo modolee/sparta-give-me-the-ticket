6f1f5a49d257ff7b2f3bea116641b847097e73a0
komiharuu <biomilky3@gmail.com>
2024-08-04 00:00:44 +0900

    ✨ Update/querybuilder and index

--- src/modules/shows/shows.service.ts
+++ src/modules/shows/shows.service.ts
@@ -37,6 +37,7 @@ export class ShowsService {
   constructor(
     @InjectRepository(Show) private showRepository: Repository<Show>,
     @InjectRepository(Bookmark) private bookmarkRepository: Repository<Bookmark>,
+    @InjectRepository(Ticket) private ticketRepository: Repository<Ticket>,
     @InjectRepository(Image) private imagesRepository: Repository<Image>,
     @InjectQueue(QUEUES.TICKET_QUEUE) private ticketQueue: Queue,
     private readonly ticketQueueEvents: TicketQueueEvents,
@@ -405,10 +406,7 @@ export class ShowsService {
 
       // 스케줄이 있는지 확인합니다.
       const schedule = await queryRunner.manager.findOne(Schedule, {
-        where: {
-          id: scheduleId,
-          showId: showId,
-        },
+        where: [{ id: scheduleId }, { showId: showId }],
       });
 
       if (!schedule) {
@@ -418,6 +416,18 @@ export class ShowsService {
       //지정 좌석이 있는지 확인합니다.
       if (schedule.remainSeat <= SHOW_TICKETS.COMMON.SEAT.UNSIGNED) {
         throw new ConflictException(SHOW_TICKET_MESSAGES.COMMON.SEAT.NOT_ENOUGH);
+      }
+
+      // 한 사람은 한 공연당 최대 5개의 티켓을 구매할 수 있게 합니다.
+      const userId = user.id;
+      const ticketLimit = await this.ticketRepository
+        .createQueryBuilder(SHOW_TICKETS.COMMON.TICKET.COUNT.TABLE)
+        .where(SHOW_TICKETS.COMMON.TICKET.COUNT.USER, { userId })
+        .andWhere(SHOW_TICKETS.COMMON.TICKET.COUNT.SHOW, { showId })
+        .getCount();
+
+      if (ticketLimit >= SHOW_TICKETS.COMMON.TICKET.COUNT.MAX) {
+        throw new ConflictException(SHOW_TICKET_MESSAGES.COMMON.TICKET.MAXIMUM);
       }
 
       //date와 time을 하나의 showTime으로 연결합니다.
