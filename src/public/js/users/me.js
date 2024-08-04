b2e017f7534062a8f6658bec8b5ee41b51d6eb31
xszvvfm <bce0102@gmail.com>
2024-08-04 18:57:18 +0900

    Add/getUserProfile ejs 구현

--- src/public/js/users/me.js
+++ src/public/js/users/me.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 document.addEventListener('DOMContentLoaded', function () {
   const myProfile = document.querySelector('#myProfile');
   const profileContent = document.querySelector('#profileContent');
@@ -9,17 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const token = window.localStorage.getItem('accessToken');
 
-  //----------- my profile ---------------------
-  myProfile.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    const h1 = myProfile.closest('li').parentNode.previousSibling;
-    h1.textContent = 'MY PROFILE';
-    myProfile.parentElement.style.opacity = '1';
-    Array.from(myProfile.parentElement.parentElement.children).forEach(function (sibling) {
-      if (sibling !== myProfile.parentElement) sibling.style.opacity = '.6';
-    });
-  });
+  // 내 정보 (profile)를 기본으로 가져옴
+  getUserProfile();
 
   function showContent(content) {
     profileContent.style.display = 'none';
@@ -27,24 +16,52 @@ document.addEventListener('DOMContentLoaded', function () {
     content.style.display = 'block';
   }
 
-  myProfile.addEventListener('click', async (e) => {
+  //----------- my profile ---------------------
+  myProfile.addEventListener('click', function (e) {
     e.preventDefault();
-    showContent(profileContent);
 
+    const h1 = myProfile.closest('li').parentNode.previousSibling;
+    myProfile.parentElement.style.opacity = '1';
+    Array.from(myProfile.parentElement.parentElement.children).forEach(function (sibling) {
+      if (sibling !== myProfile.parentElement) sibling.style.opacity = '.6';
+    });
+
+    showContent(profileContent);
+    getUserProfile();
+  });
+
+  async function getUserProfile() {
     try {
       // 백엔드 사용자 프로필 조회 API 호출
-      await axios.get('/users/me', {
+      const response = await axios.get('/users/me', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
+
+      console.log('Response data: ', response.data);
+
+      const user = response.data.getUserProfile;
+      console.log('User data: ', user);
+
+      document.getElementById('nickname').textContent = user.nickname;
+      document.getElementById('email').textContent = user.email;
+      document.getElementById('point').textContent = user.point;
+
+      if (user && user.profileImg) {
+        document.getElementById('profileImg').src = user.profileImg;
+      } else {
+        console.error('Profile image is undefined.');
+        document.getElementById('profileImg').src =
+          'https://e7.pngegg.com/pngimages/1000/665/png-clipart-computer-icons-profile-s-free-angle-sphere.png'; // 기본 이미지 URL
+      }
     } catch (err) {
       // 사용자 프로필 조회 실패 시 에러 처리
       console.log(err.response.data);
       const errorMessage = err.response.data.message;
       alert(errorMessage);
     }
-  });
+  }
 
   //----------- update user ---------------------
   updateBtn.addEventListener('click', function (e) {
